73c7e74d41b6d401acc0aeb5b31b4c49a36cc123
Stephen <59405071+I-Program-Stuff@users.noreply.github.com>
2021-03-29 22:58:38 -0400

    Create double_pointer_reverse.c

---
+++ double_pointer_reverse.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char word[] = "Word";
+    char* p1 = &word[0];
+    char* p2 = &word[strlen(word)-1];
+    if(!(strlen(word) % 2 == 0)){
+        while(p1 != &word[(strlen(word)-1)/2] && p2 != &word[(strlen(word)-1)/2]){
+            char p1_log = *p1;
+            char p2_log = *p2;
+            *p1 = p2_log;
+            *p2 = p1_log;
+            *(++p1);
+            *(--p2);
+        }
+    }else{
+        while(p1 != &word[(strlen(word)-1)/2+1] && p2 != &word[(strlen(word)-1)/2-1]){
+            char p1_log = *p1;
+            char p2_log = *p2;
+            *p1 = p2_log;
+            *p2 = p1_log;
+            *(++p1);
+            *(--p2);
+        }
+    }
+    printf("%s",word);
+}
